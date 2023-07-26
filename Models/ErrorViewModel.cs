f41ff32dac8454beff0618b8ac581034e5d3d6ed
AmaluAS <asamalu8@gmail.com>
2023-07-26 18:25:50 +0530

    Revert "Add project files."
    This reverts commit 9e447dca936a3446a2358f8fc84cd21278628709.

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TaskBookWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
